41ed1fd6c69441d893201d9b458ef57e0be2137c
Giovanna-senaisp <Giovannamendessenai@gmail.com>
2025-03-15 10:40:46 -0300

    feat: Ativadidades Java

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num01 = 10;
+        int num02 = 20;
+        int num03 = 30;
+        double media = (num01+num02+num03) /3.0;
+
+        float media2 = (num01+num02+num03)/3.0f;
+        long media3 = (num01+num02+num03)/3;
+
+
+        System.out.println("A média é: " + media);
+        System.out.println("A média do número real é: " + num01);
+        System.out.println("A média do número float é: " + media2);
+        System.out.println("A média do número long é: " + media3);
+
+
+    }
+}
