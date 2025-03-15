cb1d95dcdc3ecc54ce3fe7481c8e3be26f6dec32
Giovanna-senaisp <Giovannamendessenai@gmail.com>
2025-03-15 12:18:18 -0300

    feat: up Atividades

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,46 @@ public class Main {
         System.out.println("A média do número float é: " + media2);
         System.out.println("A média do número long é: " + media3);
 
-
+        System.out.println("--------------------------------------");
 
                 long distancia = 149600000L;
 
                 System.out.println("A distância da Terra ao Sol é: " + distancia + " km.");
+
+        System.out.println("--------------------------------------");
+
+        char letra = 'A';
+                int valorUnicode = letra;
+
+                System.out.println("O valor Unicode de '" + letra + "' é: " + valorUnicode);
+
+        System.out.println("--------------------------------------");
+
+                int numero = 15;
+                boolean maiorQueDez = numero > 10;
+
+                System.out.println("O número " + numero + " é maior que 10? " + maiorQueDez);
+
+        System.out.println("--------------------------------------");
+
+                int[] numeros = {10, 20, 30, 40, 50};
+                System.out.println("Primeiro número: " + numeros[0]);
+                System.out.println("Último número: " + numeros[numeros.length -1]);
+
+        System.out.println("--------------------------------------");
+
+                double largura = 5.0;
+                double altura = 3.0;
+                double area = largura * altura;
+
+                System.out.println("A área do retângulo é: " + area);
+
+        System.out.println("--------------------------------------");
+        
+                String nome = "Giovanna";
+                System.out.println("Olá, " + nome + "! Bem-vindo ao curso de Java.");
             }
         }
 
-        
+
+
